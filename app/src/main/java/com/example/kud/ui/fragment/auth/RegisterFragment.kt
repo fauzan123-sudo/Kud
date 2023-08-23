a8c2b249542aa180d7c87fee06f31c1e2dd03437
damha <damhadamha47@gmail.com>
2023-08-23 10:47:43 +0700

    add logo

---
+++ app/src/main/java/com/example/kud/ui/fragment/auth/RegisterFragment.kt
@@ -0,0 +1,17 @@
+package com.example.kud.ui.fragment.auth
+
+import android.os.Bundle
+import android.view.View
+import com.example.kud.databinding.FragmentRegisterBinding
+import com.example.kud.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
